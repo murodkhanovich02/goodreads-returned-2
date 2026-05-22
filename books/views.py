319383e227e99c6877f3424b18e87924d3348fd5
murodkhanovich02-hue <murodkhanovich02@gmail.com>
2026-05-22 09:22:06 +0500

    static files, TestCase, generic Views

--- books/views.py
+++ books/views.py
@@ -1,24 +1,36 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from django.views import View
 
 from .models import Books
 
 
-class BookListView(View):
-
-    def get(self, request):
-        books = Books.objects.all()
-        context = {
-            'books': books
-        }
-        return render(request, 'books/books.html', context)
+class BookListView(ListView):
+    template_name = 'books/books.html'
+    queryset = Books.objects.all()
+    context_object_name = 'books'
 
 
-class BookDetailView(View):
+# class BookListView(View):
+#
+#     def get(self, request):
+#         books = Books.objects.all()
+#         context = {
+#             'books': books
+#         }
+#         return render(request, 'books/books.html', context)
 
-    def get(self, request, id):
-        book = Books.objects.get(id=id)
-        context = {
-            'book': book
-        }
-        return render(request, 'books/book_detail.html', context)
+class BookDetailView(DetailView):
+    template_name = 'books/book_detail.html'
+    pk_url_kwarg = 'id'
+    model = Books
+    context_object_name = 'book'
+
+# class BookDetailView(View):
+#
+#     def get(self, request, id):
+#         book = Books.objects.get(id=id)
+#         context = {
+#             'book': book
+#         }
+#         return render(request, 'books/book_detail.html', context)
--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class RegistrationTestCase(TestCase):
+
+    def test_user_account_is_create(self):
+        self.client.post(
+            '/users/register',
+            data={
+                'username': 'admin1',
+                'first_name': 'admin1',
+                'last_name': 'admin1',
+                'email': 'admin1@gmail.com',
+                'password': 'password'
+            }
+        )
