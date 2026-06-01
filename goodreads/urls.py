71829060582f1b4143017b9ce7ace97b915e6b83
murodkhanovich02-hue <murodkhanovich02@gmail.com>
2026-06-01 09:30:38 +0500

    init

--- goodreads/urls.py
+++ goodreads/urls.py
@@ -4,12 +4,13 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-from .views import landing
+from .views import landing, home_page
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('', landing, name='landing'),
+    path('home', home_page, name='home'),
     path('users/', include('users.urls')),
     path('books/', include('books.urls')),
 ]
--- goodreads/views.py
+++ goodreads/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
 
+from books.models import Review
+
 
 def landing(request):
     return render(request, 'landing.html')
+
+
+def home_page(request):
+    book_review = Review.objects.all().order_by('-created_at')
+    context = {
+        'book_review': book_review
+    }
+    return render(request, 'home.html', context)
